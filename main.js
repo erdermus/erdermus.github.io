8e06c0760b74b676d049f3d8ccca1120f9b7dd7f
etcado <etc.turgut@gmail.com>
2023-06-03 11:13:14 +0200

    divided into seperate files

--- main.js
+++ main.js
@@ -5,20 +5,20 @@ function toggleInfo() {
     var column2 = document.getElementById('column2');
     
     if (info.style.display === 'none') {
-        info.style.display = 'block';
-        info.style.width = '300px';
-        middle.innerHTML = 'X';
         setTimeout(function() {
             column1.classList.add('small');
             column2.classList.add('small');
-        }, 250); // Timeout equal to the transition duration
+        }, 10); // Timeout equal to the transition duration
+        info.style.display = 'block';
+        info.style.width = '500px';
+        middle.innerHTML = 'X';
     } else {
+        setTimeout(function() {
+            info.style.display = 'none';
+        }, 150); // Timeout equal to the transition duration
         info.style.width = '0';
         column1.classList.remove('small');
         column2.classList.remove('small');
-        middle.innerHTML = 'Your Name';
-        setTimeout(function() {
-            info.style.display = 'none';
-        }, 250); // Timeout equal to the transition duration
+        middle.innerHTML = 'ERDEM TURGUT';
     }
 }
